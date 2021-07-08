86dacd380d49e45ff09c43a429bf4c5e33f2bd62
김상욱 <sangyook04@gmail.com>
2021-07-08 21:31:51 +0900

    백준 10171

---
+++ kswWorkSpace/src/kswWorkSpace/day_20210708_10171.java
@@ -0,0 +1,14 @@
+package kswWorkSpace;
+
+public class day_20210708_10171 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+        
+	}
+
+}
