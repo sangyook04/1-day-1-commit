95df9f19398fa74a8d8733219fa6a93de9abc074
김상욱 <sangyook04@gmail.com>
2021-07-09 23:00:07 +0900

    20210709 10172 백준

---
+++ kswWorkSpace/src/kswWorkSpace/day_20210709_10172.java
@@ -0,0 +1,15 @@
+package kswWorkSpace;
+
+public class day_20210709_10172 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+		
+	}
+
+}
