8443edaa106d231d01c07085c0bb45f07deb6ec6
김상욱 <sangyook04@gmail.com>
2021-07-10 12:43:16 +0900

    20210710 1000 백준

---
+++ kswWorkSpace/src/kswWorkSpace/day_20210710_1000.java
@@ -0,0 +1,25 @@
+package kswWorkSpace;
+
+import java.util.Scanner;
+
+public class day_20210710_1000 {
+
+	public static void main(String[] args) {
+		
+		int number1 = 0;
+		int number2 = 0;
+		int sumNumber = 0;
+		
+		
+		Scanner	sc = new Scanner(System.in);
+		
+		number1 = sc.nextInt();
+		number2 = sc.nextInt();
+		
+		sumNumber = number1 + number2;
+		
+		System.out.println(sumNumber);
+
+	}
+
+}
