4909ffff54e44c7147eab3910238977d2a8baf69
김상욱 <sangyook04@gmail.com>
2021-07-13 20:00:22 +0900

    20210713 2588 백준

---
+++ kswWorkSpace/src/kswWorkSpace/day_20210713_2588.java
@@ -0,0 +1,24 @@
+package kswWorkSpace;
+
+import java.util.Scanner;
+
+public class day_20210713_2588 {
+
+	public static void main(String[] args) {
+		
+		int firstNumber = 0;
+		String secondNumber = "";
+		
+		Scanner	sc = new Scanner(System.in);
+		
+		firstNumber = sc.nextInt();
+		secondNumber = sc.next();
+		
+		System.out.println(firstNumber*Integer.parseInt(secondNumber.substring(2,3)));
+		System.out.println(firstNumber*Integer.parseInt(secondNumber.substring(1,2)));
+		System.out.println(firstNumber*Integer.parseInt(secondNumber.substring(0,1)));
+		System.out.println(firstNumber*Integer.parseInt(secondNumber));
+
+	}
+
+}
