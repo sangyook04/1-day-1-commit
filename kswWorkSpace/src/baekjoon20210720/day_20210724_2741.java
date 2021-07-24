d7075b87e4c08bd29e7fac886af9dd1158ff81c2
sangyook04 <sangyook04@gmail.com>
2021-07-24 11:17:05 +0900

    N 찍기

---
+++ kswWorkSpace/src/baekjoon20210720/day_20210724_2741.java
@@ -0,0 +1,21 @@
+package baekjoon20210720;
+
+import java.util.Scanner;
+
+public class day_20210724_2741 {
+
+	public static void main(String[] args) {
+		
+		int number = 0;
+		
+		Scanner	sc = new Scanner(System.in);
+		
+		number = sc.nextInt();
+		
+		for(int i=1; i<=number; i++) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
