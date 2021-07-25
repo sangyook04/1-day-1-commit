354e1077ffee4df35fdf39ef224f4125eae92355
김상욱 <sangyook04@gmail.com>
2021-07-25 23:11:18 +0900

    기찍 N

---
+++ kswWorkSpace/src/baekjoon20210720/day_20210725_2742.java
@@ -0,0 +1,21 @@
+package baekjoon20210720;
+
+import java.util.Scanner;
+
+public class day_20210725_2742 {
+
+	public static void main(String[] args) {
+		
+		int number = 0;
+		
+		Scanner	sc = new Scanner(System.in);
+		
+		number = sc.nextInt();
+		
+		for(int i=number; i>=1; i--) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
