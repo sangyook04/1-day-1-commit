67c1d6220f3d8f8ff5aae3879b32be2e2d616482
sangyook04 <sangyook04@gmail.com>
2021-08-03 19:22:31 +0900

    더하기사이클

--- kswWorkSpace/src/baekjoon20210720/day_20210802_1110.java
+++ kswWorkSpace/src/baekjoon20210720/day_20210802_1110.java
@@ -7,13 +7,16 @@ public class day_20210802_1110 {
 	public static void main(String[] args) {
 		
 		int number1 = 0;
+		String number2 = "";
 		String number1String = "";
+		String number1String3 = "";
 		int numberResult = 0;
-		int count = 0;
+		int count = 1;
 		
 		Scanner	sc = new Scanner(System.in);
 		
 		number1 = sc.nextInt();
+		numberResult = number1;
 		
 		number1String = Integer.toString(number1);
 		
@@ -21,21 +24,29 @@ public class day_20210802_1110 {
 			number1String = "0"+number1String;
 		}
 		
-		//while(sc.hasNextInt()) {
+			
 		while(true) {
 			
-			System.out.println(Integer.parseInt(number1String.substring(0, 1)) + Integer.parseInt(number1String.substring(1, 2)));
+			number1String3 = Integer.toString(Integer.parseInt(number1String.substring(0, 1)) + Integer.parseInt(number1String.substring(1, 2)));
 			
-			if(number1 == numberResult) {
-				
-				System.out.println(count);
-				
-				break;
+			if(Integer.parseInt(number1String3) < 10) {
+				number1String3 = "0"+number1String3;
 			}
 			
-			count++;
+			number2 = number1String.substring(1, 2) + number1String3.substring(1,2);
+			
+			if(Integer.parseInt(number2) == numberResult) {
+				break;
+			} else {
+				count++;
+			}
+			
+			number1String3 = number2;
+			number1String = number1String3;
 			
 		}
+		
+		System.out.println(count);
 		
 	}
 
