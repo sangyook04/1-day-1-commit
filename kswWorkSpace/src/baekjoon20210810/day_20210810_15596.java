68a40824e968522ccf9f46080c5374d39f8bed2b
김상욱 <sangyook04@gmail.com>
2021-08-10 20:56:29 +0900

    정수 N개의 합

---
+++ kswWorkSpace/src/baekjoon20210810/day_20210810_15596.java
@@ -0,0 +1,20 @@
+package baekjoon20210810;
+
+public class day_20210810_15596 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
+
+class Test {
+    long sum(int[] a) {
+		long sum = 0;
+        
+		for(int i = 0; i < a.length; i++) {
+			sum += a[i];
+		}
+		return sum;
+	}
+}
