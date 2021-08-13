36c38d61ad8599fed353927b092f9d1aa0d5a04f
김상욱 <sangyook04@gmail.com>
2021-08-14 00:00:44 +0900

    숫자의 합

---
+++ kswWorkSpace/src/baekjoon20210810/day_20210814_11720.java
@@ -0,0 +1,24 @@
+package baekjoon20210810;
+
+import java.util.Scanner;
+
+public class day_20210814_11720 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int length = sc.nextInt();
+		String number = sc.next();
+		int sum = 0;
+		
+		for(int i = 0; i < length; i++) {
+			sum += Character.getNumericValue(number.charAt(i));
+		}
+		
+		System.out.println(sum);
+		
+	}
+
+}
+
