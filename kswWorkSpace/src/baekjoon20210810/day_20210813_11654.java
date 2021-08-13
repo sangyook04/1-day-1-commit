02b4fc5ff81df907e9d0ea1134e757ef6bd29737
김상욱 <sangyook04@gmail.com>
2021-08-13 20:14:53 +0900

    아스키코드

---
+++ kswWorkSpace/src/baekjoon20210810/day_20210813_11654.java
@@ -0,0 +1,20 @@
+package baekjoon20210810;
+
+import java.util.Scanner;
+
+public class day_20210813_11654 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		char asc = sc.nextLine().charAt(0);
+		
+		int num = (int)asc;
+		
+		System.out.println(num);
+		
+	}
+
+}
+
