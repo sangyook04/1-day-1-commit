dd116539ff5619593d8f15c5ce74d1135978af09
sangyook04 <sangyook04@gmail.com>
2021-08-17 08:56:42 +0900

    알파벳 찾기

--- kswWorkSpace/src/baekjoon20210810/day_20210815_10809.java
+++ kswWorkSpace/src/baekjoon20210810/day_20210815_10809.java
@@ -18,7 +18,7 @@ public class day_20210815_10809 {
  
 		for(int i = 0; i < S.length(); i++) {
 			char ch = S.charAt(i);
-    
+
 			//아스키 코드 a의 값만큼 빼줘서 문자가 b일 경우 b의 위치를 1로 탐색해준다
 			if(arr[ch - 'a'] == -1) {	
 				arr[ch - 'a'] = i;
@@ -28,6 +28,7 @@ public class day_20210815_10809 {
 		for(int val : arr) {	
 			System.out.print(val + " ");
 		}
+		
 	}
 
 }
