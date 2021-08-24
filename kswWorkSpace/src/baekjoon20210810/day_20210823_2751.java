f2f1a6076648ca88eb5e17b54679e637e05f7a4d
김상욱 <sangyook04@gmail.com>
2021-08-24 16:30:23 +0900

    숫자 정렬하기

---
+++ kswWorkSpace/src/baekjoon20210810/day_20210823_2751.java
@@ -0,0 +1,32 @@
+package baekjoon20210810;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class day_20210823_2751 {
+	 
+	public static void main(String[] args) {
+ 
+		Scanner sc = new Scanner(System.in);
+		
+		StringBuilder sb = new StringBuilder();
+		
+		int N = sc.nextInt();
+		
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		for(int i = 0; i < N; i++) {
+			list.add(sc.nextInt());
+		}
+		
+		Collections.sort(list);
+		
+		for(int value : list) {
+			sb.append(value).append('\n');
+		}
+		System.out.println(sb);
+		
+	}
+ 
+}
