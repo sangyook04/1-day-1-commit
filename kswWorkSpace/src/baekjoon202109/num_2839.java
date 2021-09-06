d3fcd82e3a457ad3823808ffab62feda263f025e
김상욱 <sangyook04@gmail.com>
2021-09-06 20:48:07 +0900

    설탕

---
+++ kswWorkSpace/src/baekjoon202109/num_2839.java
@@ -0,0 +1,17 @@
+package baekjoon202109;
+
+import java.util.Scanner;
+
+public class num_2839 {
+		 
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int sugar = sc.nextInt();
+		
+		
+		
+	}
+	
+}
